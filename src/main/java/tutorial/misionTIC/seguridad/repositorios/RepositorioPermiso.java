7940ab5e9454836d35ab34c39887b9c06bf5a2dd
acidbeer <112288217+acidbeer@users.noreply.github.com>
2022-11-13 12:41:19 -0500

    funciones de validacion

--- src/main/java/tutorial/misionTIC/seguridad/repositorios/RepositorioPermiso.java
+++ src/main/java/tutorial/misionTIC/seguridad/repositorios/RepositorioPermiso.java
@@ -1,8 +1,12 @@
 package tutorial.misionTIC.seguridad.repositorios;
 
+import org.springframework.data.mongodb.repository.Query;
 import tutorial.misionTIC.seguridad.modelos.Permiso;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RepositorioPermiso extends MongoRepository<Permiso,String> {
+
+    @Query("{'url': ?0, 'metodo': ?1}")
+    public Permiso findByurlAndMethod(String url, String metodo);
 
 }
--- src/main/java/tutorial/misionTIC/seguridad/repositorios/RepositorioUsuario.java
+++ src/main/java/tutorial/misionTIC/seguridad/repositorios/RepositorioUsuario.java
@@ -1,9 +1,13 @@
 package tutorial.misionTIC.seguridad.repositorios;
 
+import org.springframework.data.mongodb.repository.Query;
 import tutorial.misionTIC.seguridad.modelos.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RepositorioUsuario  extends MongoRepository<Usuario,String>{
+
+    @Query("{'correo' : ?0}")
+    public Usuario findByEmail(String correo);
 
 
 }
